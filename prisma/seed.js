bdf44d00b199575f40f04aadabc23cf525f95ed0
Hubert Lemczak <hubertlemczak@gmail.com>
2022-08-26 12:57:43 +0100

    req 2

--- prisma/seed.js
+++ prisma/seed.js
@@ -2,26 +2,30 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 async function seed() {
-    const createdCustomer = await prisma.customer.create({
-        data: {
-            name: 'Alice'
-        }
-    });
+  const createdCustomer = await prisma.customer.create({
+    data: {
+      name: 'Alice',
+      contact: {
+        create: {
+          phone: '123123123123',
+          email: 'email@email.com',
+        },
+      },
+    },
+    include: {
+      contact: true,
+    },
+  });
 
-    console.log('Customer created', createdCustomer);
+  console.log('Customer created', createdCustomer);
 
-    // Add your code here
-
-
-
-
-    // Don't edit any of the code below this line
-    process.exit(0);
+  // Add your code here
+  // Don't edit any of the code below this line
+  process.exit(0);
 }
 
-seed()
-    .catch(async (error) => {
-        console.error(error);
-        await prisma.$disconnect();
-        process.exit(1);
-    })
+seed().catch(async error => {
+  console.error(error);
+  await prisma.$disconnect();
+  process.exit(1);
+});
