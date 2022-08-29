4c5e9d717770a5a956fa6542253c4a6767c84780
Hubert Lemczak <hubertlemczak@gmail.com>
2022-08-29 22:15:01 +0100

    c

--- prisma/seed.js
+++ prisma/seed.js
@@ -5,7 +5,7 @@ async function seed() {
   const createdCustomer = await prisma.customer.create({
     data: {
       name: 'Alice',
-      contact: {
+      Contact: {
         create: {
           phone: '123123123123',
           email: 'email@email.com',
@@ -13,11 +13,44 @@ async function seed() {
       },
     },
     include: {
-      contact: true,
+      Contact: true,
     },
   });
 
   console.log('Customer created', createdCustomer);
+
+  const createdMovie = await prisma.movie.create({
+    data: {
+      title: 'Alice in Wonderland',
+      runtimeMins: 108,
+    },
+  });
+
+  console.log('Movie created', createdMovie);
+
+  const createdScreen = await prisma.screen.create({
+    data: {
+      number: 1,
+      screenings: {
+        create: {
+          startsAt: new Date('2022-09-02T20:23:33+00:00'),
+          movieId: 1,
+        },
+      },
+    },
+    include: {
+      screenings: true,
+    },
+  });
+
+  console.log('Screen created', createdScreen);
+
+  const createdTicket = await prisma.ticket.create({
+    data: {
+      screeningId: 1,
+      customerId: 1,
+    },
+  });
 
   // Add your code here
   // Don't edit any of the code below this line
